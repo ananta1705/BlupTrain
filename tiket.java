8e4fa901452b2cd692427c02e0367678ce61e327
ANANTA PUTI MAHARANI <your@example.com>
2024-12-30 21:19:25 +0700

    baru

--- tiket.java
+++ tiket.java
@@ -2,16 +2,22 @@ import java.util.Scanner;
 public class tiket {
     public static void main(String[] args) {
         int pilihan;
+        int harga = 60000;
+        int harga1 = 120000;
+        int harga2 = 85000;
         int nik = 0;
         String nama = "";
         String email = "";
+        int bayi;
+        String lanjut = "";
+       
         Scanner s = new Scanner (System.in);
-        System.out.println("<<<<<<SELAMAT DATANG di BLUP TRAIN>>>>>>\n");
+        System.out.println("<<<<<<SELAMAT DATANG DI BLUP TRAIN>>>>>>\n");
         System.out.print(" 1. create\n 2. read\n 3. update\n 4. delete\n");
-        System.out.print("Pilih no berapa: ");
+        System.out.print("\nPilih no berapa: ");
         pilihan = s.nextInt();
         s.nextLine();// Membersihkan buffer setelah nextInt()
-        clearScreen(); // Membersihkan layar setelah input data
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n");
         if (pilihan == 1){
             System.out.println("<<<<<<< REGISTRASI >>>>>>>>>>>");
             System.out.print("Nama: ");
@@ -21,53 +27,103 @@ public class tiket {
             s.nextLine(); // Membersihkan buffer setelah nextInt()
             System.out.print("Email: ");
             email = s.nextLine();
-            clearScreen(); // Membersihkan layar setelah input data
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n");
+
             System.out.println("<<<<<< KATEGORI >>>>>>");
+            System.out.println("list asal dan tujuan:\n1. Purwokerto -> Purworejo\n2. Purwokerto -> Yogyakarta\n3. Purworejo -> Purwokerto\n4. Yogyakarta -> Purwokerto\n5. Purworejo -> Yogyakarta\n6. Yogyakarta -> Purworejo\n");
             System.out.print("Asal stasiun: ");
             String asal = s.nextLine();
             System.out.print("Tujuan stasiun: ");
             String tujuan = s.nextLine();
-            clearScreen(); // Membersihkan layar setelah input data
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n");
+
             System.out.println("------ Jadwal Keberangkatan ------");
             System.out.print("Masukkan tanggal: ");
             int tanggal = s.nextInt();
-            System.out.print("Masukkan bulan (huruf): ");
+            System.out.print("Masukkan bulan (angka): ");
             int bulan = s.nextInt();
             System.out.print("Masukkan tahun: ");
             int tahun = s.nextInt();
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n");
+
             System.out.print("jam keberangkatan:\n1. 08.00\n2. 12.00\n3. 16.00\njam berapa:  ");
             int jam = s.nextInt();
-            clearScreen();
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n");
+
             System.out.println("<<<<<<< KERETA DAN KURSI >>>>>>");
-            System.out.print("Pilih kereta:\n1. Ekonomi\n2. eksekutive\n3. bisnis\npilih nomor berapa: ");
+            System.out.print("Pilih kereta:\n1. Ekonomi\n2. eksekutive\npilih nomor berapa: ");
             int kereta = s.nextInt();
-            clearScreen();
-            System.out.print("kursi yang tersedia:\n1. 1A\n2. 8D\n3. 9C\n4. 12B\n pilih omor berapa: ");
-            int kursi = s.nextInt();
-            clearScreen();
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n");
+
             System.out.println("<<<<<< ISI DATA PENUMPANG >>>>>>");
             System.out.print("Dewasa: ");
             int dewasa = s.nextInt();
-            System.out.print("Bayi: ");
-            int bayi = s.nextInt();
-            System.out.println("<<<<<< PEMBAYARAN >>>>>>");
-            System.out.print("pembayaran menggunakan E-wallet\n1. Dana\n2. Ovo\n3. Linkaja\n4. BlupTrainpay\n5. m-banking\npilih apa (inputkan nomornya): ");
-            int pembayaran = s.nextInt();
-            clearScreen();
-            System.out.println("<<<<<< E-TICKET >>>>>>");
-            System.out.print("E-Ticket anda sudah dikirim ke Email anda");
-        }else if (pilihan == 2){
-
-        }else if (pilihan == 3){
-
-        }else if (pilihan == 4){
-
-        }else{
-            System.out.println("pilihan tidak tersedia");
+            if (kereta == 1){
+                harga = harga * dewasa;
+                System.out.print("Anak (umur diatas 3 tahun): ");
+                bayi = s.nextInt();
+                harga = (harga * dewasa) + (harga * bayi);
+                tiket obj = new tiket();
+                obj.read(nama, nik, email, asal, tujuan, tanggal, bulan, tahun, jam, dewasa, bayi, harga, kereta);
+                
+            }else{
+            harga = 2 * harga * dewasa;
+            System.out.print("Anak (umur diatas 3 tahun): ");
+            bayi = s.nextInt();
+            harga = 2 * ((harga * dewasa) + (harga * bayi));
+            tiket obj = new tiket();
+            obj.read(nama, nik, email, asal, tujuan, tanggal, bulan, tahun, jam, dewasa, bayi, harga, kereta);
+                
+            }
+            
+                
+            
         }
     
-    }public static void clearScreen() {
-        System.out.print("\033[H\033[2J"); // Escape sequence ANSI untuk membersihkan layar
-        System.out.flush();
     }
-}
+    
+     static void read (String nama, int nik, String email, String asal, String tujuan, int tanggal, int bulan, int tahun, int jam, int dewasa, int bayi, int harga, int kereta){
+        char lanjut;
+        Scanner s = new Scanner (System.in);
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("<<<<<<< REGISTRASI >>>>>>>>>>>");
+        System.out.print("\nNama: "+ nama);
+        System.out.print("\nNik: "+nik);
+        System.out.print("\nEmail: "+ email);
+        System.out.print("\nAsal Stasiun: "+asal);
+        System.out.print("\ntujuan Stasiun: "+ tujuan);
+        System.out.print("\nTanggal Keberangkatan: " + tanggal + "/" + bulan + "/" + tahun);
+        
+        if (jam==1) {
+            System.out.print("\nJam Keberangkatan: 08.00");
+        }else if (jam==2) {
+            System.out.print("\nJam Keberangkatan: 12.00"); 
+        }else if (jam==3) {
+            System.out.print("\nJam Keberangkatan: 16.00");
+        }else{
+            System.out.print("\nJam Keberangkatan: 20.00");
+        }
+        System.out.print("\nKategori: "+ kereta);
+        System.out.println("\nTiket Dewasa: "+ dewasa + "\nTiket Anak: " + bayi);
+        System.out.println("Total tiket: " + (dewasa + bayi));
+        System.out.print("\nTotal = Rp." + harga);
+        System.out.println("\nPembayaran menggunakan BlupTrainPay\nQr pembayaran akan dikirim ke email anda");
+        System.out.println("----------------------------------------------");
+        System.out.print("Lanjutkan? (Y/N): ");
+        lanjut = s.next().charAt(0);
+        if (lanjut == 'Y' || lanjut == 'y'){
+            System.out.print("apakah sudah melakukan pembayaran (Y/N): ");
+            char bayar = s.next().charAt(0);
+            if (bayar == 'Y' || bayar == 'y'){
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n");
+                System.out.println("<<<<<< E-TICKET >>>>>>");
+                System.out.print("E-Ticket anda sudah dikirim ke Email anda");
+            }else{
+                System.out.println("silahkan melakukan pembayaran terlebih dahulu");
+            }
+        }else{
+            System.out.println("baik, terimaksih\njika sudah siap untuk dilanjutkan silahkan melakukan registrasi ulang");
+        }
+        
+    }
+    }
